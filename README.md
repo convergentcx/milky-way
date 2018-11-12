0f824d386cd8d0158123cd05ec2c85d21c176c45
lsaether <Lsaether@protonmail.com>
2018-11-12 11:01:50 +0100

    move repo

--- README.md
+++ README.md
@@ -10,3 +10,28 @@ Clone this repository into a local directory then run `yarn` to install the remo
 `yarn generate` to compile the smart contracts and create Truffle artifacts with TypeScript bindings. 
 In one window run `yarn ganache` to start the development chain. In another window run `yarn test` to 
 run the tests.
+
+## Running the Demo
+
+Start by installing dependencies and running ganache in one console:
+
+```
+$ yarn
+$ yarn ganache
+$ 
+```
+
+In other console run:
+
+```
+$ yarn compile:dapp
+```
+
+Remaining in the new console, move to the `dapp/` directory and run the following:
+
+```
+$ yarn
+$ yarn start
+```
+
+Navigate to `http://localhost:3000` in a web browser.
--- dapp/src/App.js
+++ dapp/src/App.js
@@ -15,7 +15,9 @@ import * as Web3 from 'web3';
 
 const w3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8577'));
 
-const CBT = new w3.eth.Contract(require('./SimpleCBT.json').abi)
+const artifact = require('../build/contracs/SimpleCBTDetailed.json');
+
+const CBT = new w3.eth.Contract(artifact.abi)
 
 // Out of 1 million
 const reserveRatio = 500000;
@@ -43,7 +45,7 @@ class App extends Component {
   async componentWillMount() {
     const account = (await w3.eth.getAccounts())[0];
     const balance = await w3.eth.getBalance(account);
-    const cbt = await CBT.deploy({ data: require('./SimpleCBT.json').bytecode, arguments: [ reserveRatio ] }).send({ from: account, gasPrice: '12', gas: '6000000' });
+    const cbt = await CBT.deploy({ data: artifact.bytecode, arguments: [ reserveRatio ] }).send({ from: account, gasPrice: '12', gas: '6000000' });
     const range = [...new Array(300).keys()].slice(1).map(val => w3.utils.toWei(String(val), 'ether'));
     const data = await Promise.all(range.map(async val => new Object({name: w3.utils.fromWei(val, 'ether'), uv: w3.utils.fromWei(await cbt.methods.calculateCurvedMintReturn(val).call(), 'ether')})));
     const poolBalance = await cbt.methods.poolBalance().call();
